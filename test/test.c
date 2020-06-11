84996d29db4c5001cb1f46c10670021379142b55
rumeone <yuaroslav_29@mail.ru>
2020-06-11 22:12:08 +0700

    Add tests for circle_perimeter and area

--- test/test.c
+++ test/test.c
@@ -15,3 +15,46 @@ CTEST(circle, circle_perimeter)
 
     ASSERT_DBL_NEAR(expected, result);
 }
+
+CTEST(circle, circle_perimeter_bad)
+{
+    // Given
+    FILE* f = fopen("Print", "w");
+    circle c;
+    c.r = -1;
+    // When
+    float result = perimeter_circle(&c, f);
+    // Then
+    const float expected = -1;
+
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+CTEST(circle, area_circle)
+{
+    // Given
+    FILE* f = fopen("Print", "w");
+    circle c;
+    c.r = 1;
+    // When
+    float result = area_circle(&c, f);
+    // Then
+    const float expected = M_PI;
+
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+CTEST(circle, area_circle_bad)
+{
+    // Given
+    FILE* f = fopen("Print", "w");
+    circle c;
+    c.r = -1;
+    // When
+    float result = area_circle(&c, f);
+    // Then
+    const float expected = -1;
+
+    ASSERT_DBL_NEAR(expected, result);
+}
+
