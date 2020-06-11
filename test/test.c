44ae5415decff584756455938d84ea2837e07842
rumeone <yuaroslav_29@mail.ru>
2020-06-11 22:15:28 +0700

    Add tests for intersection

--- test/test.c
+++ test/test.c
@@ -58,3 +58,71 @@ CTEST(circle, area_circle_bad)
     ASSERT_DBL_NEAR(expected, result);
 }
 
+CTEST(circle, inter) {
+	// Given
+	FILE* f = fopen("Print", "w");
+
+	circle c1;
+	circle c2;
+
+	c1.r = 1;
+	c2.r = 2;
+
+    c1.C.x = 1;
+    c1.C.y= 1;
+    c2.C.x = 2;
+    c2.C.y = 2;
+
+	// When
+	int result = inter_cir(&c1, &c2, f);
+	// Then 
+	const int expected = 1;
+
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(circle, inter_bad_1s) {
+	// Given
+	FILE* f = fopen("Print", "w");
+
+	circle c1;
+	circle c2;
+
+	c1.r = -1;
+	c2.r = 2;
+
+    c1.C.x = 1;
+    c1.C.y= 1;
+    c2.C.x = 2;
+    c2.C.y = 2;
+
+	// When
+	int result = inter_cir(&c1, &c2, f);
+	// Then 
+	const int expected = -1;
+
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(circle, inter_bad_2) {
+	// Given
+	FILE* f = fopen("Print", "w");
+
+	circle c1;
+	circle c2;
+
+	c1.r = 1;
+	c2.r = -2;
+
+    c1.C.x = 1;
+    c1.C.y= 1;
+    c2.C.x = 2;
+    c2.C.y = 2;
+
+	// When
+	int result = inter_cir(&c1, &c2, f);
+	// Then 
+	const int expected = -1;
+
+	ASSERT_EQUAL(expected, result);
+}
