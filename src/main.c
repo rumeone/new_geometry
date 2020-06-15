cfb7c5ca4de2406eb7551b6d2b2f7e860a079a45
rumeone <yuaroslav_29@mail.ru>
2020-06-15 16:42:54 +0700

    CLang main.c

--- src/main.c
+++ src/main.c
@@ -11,21 +11,17 @@ int main()
     triangle t;
 
     enter_coordinate_circle(&c);
-
     print_circle(&c, f);
     perimeter_circle(&c, f);
     area_circle(&c, f);
-
     enter_coordinate_circle(&c1);
-    
     inter_cir(&c, &c1, f);
-    
-    enter_triangle_coordinate(&t);
-    print_triangle(&t,f);
-    check_triangle(&t,f);
-    perimeter_triangle(&t,f);
-    area_triangle(&t,f);
 
+    enter_triangle_coordinate(&t);
+    print_triangle(&t, f);
+    check_triangle(&t, f);
+    perimeter_triangle(&t, f);
+    area_triangle(&t, f);
     inter_triangle_circle(&t, &c, f);
 
     fclose(f);
