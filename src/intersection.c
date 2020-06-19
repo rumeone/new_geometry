e8654e1c13d14209a78f5ab78a24fa9b073dd3d1
rumeone <yuaroslav_29@mail.ru>
2020-06-20 01:20:23 +0700

    fix intersection for tests

--- src/intersection.c
+++ src/intersection.c
@@ -9,9 +9,10 @@ int inter_triangle_circle(triangle* t, circle* c, FILE* f)
         return -1;
     }
     if (c->r > 0) {
-        distance_one = sqrt(((t->T[1].x - c->C.x) * (t->T[1].x - c->C.x)) - ((t->T[1].y - c->C.y) * (t->T[1].y - c->C.y)));
-        distance_two = sqrt(((t->T[2].x - c->C.x) * (t->T[2].x - c->C.x)) - ((t->T[2].y - c->C.y) * (t->T[2].y - c->C.y)));
-        distance_three = sqrt(((t->T[3].x - c->C.x) * (t->T[3].x - c->C.x)) - ((t->T[3].y - c->C.y) * (t->T[3].y - c->C.y)));
+        distance_one = sqrt(((t->T[1].x - c->C.x) * (t->T[1].x - c->C.x)) + ((t->T[1].y - c->C.y) * (t->T[1].y - c->C.y)));
+        distance_two = sqrt(((t->T[2].x - c->C.x) * (t->T[2].x - c->C.x)) + ((t->T[2].y - c->C.y) * (t->T[2].y - c->C.y)));
+        distance_three = sqrt(((t->T[3].x - c->C.x) * (t->T[3].x - c->C.x)) + ((t->T[3].y - c->C.y) * (t->T[3].y - c->C.y)));
+        printf("distance : %f %f %f\n", distance_one, distance_two, distance_three);
         if ((distance_one < c->r) || (distance_two < c->r) || (distance_three < c->r)) {
             printf("Окружность и тругольник пересекаются\n");
             fprintf(f, "Окружность и тругольник пересекаются\n");
