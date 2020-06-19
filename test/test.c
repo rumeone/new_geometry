6dff8d4ee54bc61f01aed5e1819e8eef1b86578b
rumeone <yuaroslav_29@mail.ru>
2020-06-19 22:55:01 +0700

    Add first test for triangle

--- test/test.c
+++ test/test.c
@@ -72,7 +72,6 @@ CTEST(circle, inter) {
     c1.C.y= 1;
     c2.C.x = 2;
     c2.C.y = 2;
-
 	// When
 	int result = inter_cir(&c1, &c2, f);
 	// Then 
@@ -95,7 +94,6 @@ CTEST(circle, inter_bad_1s) {
     c1.C.y= 1;
     c2.C.x = 2;
     c2.C.y = 2;
-
 	// When
 	int result = inter_cir(&c1, &c2, f);
 	// Then 
@@ -118,11 +116,30 @@ CTEST(circle, inter_bad_2) {
     c1.C.y= 1;
     c2.C.x = 2;
     c2.C.y = 2;
-
 	// When
 	int result = inter_cir(&c1, &c2, f);
 	// Then 
 	const int expected = -1;
 
 	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(triangle, perimeter_triangle) {
+    // Given
+    FILE *f=fopen("Print", "w");
+
+    triangle t;
+    t.T[1].x=1;
+    t.T[1].y=1;
+    t.T[2].x=2;
+    t.T[2].y=2;
+    t.T[3].x=3;
+    t.T[3].y=1;
+    // When
+    float result = perimeter_triangle(&t, f);
+    // Then 
+    const float expected = 4.83;
+    ASSERT_DBL_NEAR_TOL(expected, result, 0.1);
+
+
 }
